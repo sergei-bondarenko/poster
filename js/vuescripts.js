da2cf47eae500bf1cde421f2cbaddcff3837b363
grez911 <grez911@gmail.com>
2017-10-03 15:48:15 +0300

    added picture upload

---
+++ js/vuescripts.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    methods: {
+        picUpload: function (event) {
+            let file = document.getElementById('input_file').files[0];
+            let fr = new FileReader();
+            fr.readAsDataURL(file);
+            fr.onload = () => {
+                base64 = fr.result.split(',')[1];
+                page.cmd("fileWrite", ["uploads/" + file.name, base64], (res) => {
+                    if (res == "ok") {
+                        console.log("File uploaded!");
+                    } else {
+                        page.cmd("wrapperNotification",
+                            ["error", "File write error: " + res]);
+                    }
+                });
+            };
+        }
+    }
+})
