e19d64608f32ff7bb69cc25f4ed38508ce87e974
grez911 <grez911@gmail.com>
2017-10-19 10:31:50 +0300

    Created top component

---
+++ js/top.js
@@ -0,0 +1,16 @@
+Vue.component('top', {
+    template: `
+        <section class="hero is-warning">
+            <div class="hero-body">
+                <div class="container">
+                    <p class="title">
+                        Poster
+                    </p>
+                    <p class="subtitle">
+                        Simple blog for zeronet
+                    </p>
+                </div>
+            </div>
+        </section>
+    `
+});
