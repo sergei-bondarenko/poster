79be87ab374d7f90d162fa5c3badef33a6ef8130
grez911 <grez911@gmail.com>
2017-10-19 17:22:20 +0300

    Working on dbschema

--- js/newpost.js
+++ js/newpost.js
@@ -1,13 +1,16 @@
 Vue.component('newpost', {
     template: `
         <div id="new-post">
-            <input type="file" id="input-file" @change="picUpload">
+            <input type="file" id="input-file" @change="picUpload" ref="inputFile">
+            <button type="button" @click="save">Save</button>
+            <textarea ref="postarea" id="postarea">
+            </textarea>
         </div>
     `,
 
     methods: {
         picUpload: function (event) {
-            let file = document.getElementById('input_file').files[0];
+            let file = this.$refs.inputFile.files[0];
             let fr = new FileReader();
             fr.readAsDataURL(file);
             fr.onload = () => {
@@ -15,12 +18,21 @@ Vue.component('newpost', {
                 page.cmd("fileWrite", ["uploads/" + file.name, base64], (res) => {
                     if (res == "ok") {
                         console.log("File uploaded!");
+                        this.appendImage(file.name);
                     } else {
                         page.cmd("wrapperNotification",
                             ["error", "File write error: " + res]);
                     }
                 });
             };
+        },
+
+        appendImage: function (name) {
+            this.$refs.postarea.value += "![](uploads/" + name + ")";
+        },
+
+        save: function () {
+            console.log('save');
         }
     }
 });
--- js/page.js
+++ js/page.js
@@ -1,16 +1,16 @@
 class Page extends ZeroFrame {
-/*    setSiteInfo(site_info) {
-        var out = document.getElementById("out")
-        out.innerHTML =
-            "Page address: " + site_info.address +
-            "<br>- Peers: " + site_info.peers +
-            "<br>- Size: " + site_info.settings.size +
-            "<br>- Modified: " + (new Date(site_info.content.modified*1000))
+    setSiteInfo(site_info) {
+        //var out = document.getElementById("out")
+        //out.innerHTML =
+            //"Page address: " + site_info.address +
+            //"<br>- Peers: " + site_info.peers +
+            //"<br>- Size: " + site_info.settings.size +
+            //"<br>- Modified: " + (new Date(site_info.content.modified*1000))
     }
-*/
+
     onOpenWebsocket() {
         this.cmd("siteInfo", [], function(site_info) {
-            //page.setSiteInfo(site_info)
+            page.setSiteInfo(site_info)
         })
     }
 
