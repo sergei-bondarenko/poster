5c212330484779f6d75f2671c49ed3d12530e329
grez911 <grez911@gmail.com>
2017-10-20 14:01:44 +0300

    Show newpost form only for owner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+let page = new ZeroFrame();
+
 var app = new Vue({
-    el: '#root'
+    el: '#root',
+    data() {
+        return {
+            own: false,
+        }
+    },
+    mounted() {
+        page.cmdp('siteInfo', []).then((site_info) => {
+            this.own = site_info.settings.own;
+        });
+    }
 })
