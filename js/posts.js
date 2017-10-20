e822154fb387fa6c7abb173115c5c7614f97bbb8
grez911 <grez911@gmail.com>
2017-10-20 16:38:09 +0300

    Added loading posts from database

--- js/posts.js
+++ js/posts.js
@@ -1,5 +1,37 @@
 Vue.component('posts', {
     template: `
+        <div>
+            <div v-for="post in posts" class="post">
+                <div class="columns">
+                    <div class="column is-8 post-body has-text-centered">
+                        <img src="uploads/test.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+
+    data() {
+        return {
+            comments: [],
+            posts: []
+        };
+    },
+
+    mounted() {
+        page.cmdp('dbQuery', ["SELECT * FROM post"]).then((res) => {
+            res.forEach((post) => {
+                this.posts.push({
+                    post_id: post['post_id'],
+                    body: post['body'],
+                    date_published: post['date_published']
+                });
+            });
+        });
+    }
+});
+
+/*
         <div class="post">
             <div class="columns">
                 <div class="column is-8 post-body has-text-centered">
@@ -47,5 +79,4 @@ Vue.component('posts', {
                 </div>
             </div>
         </div>
-    `
-});
+*/
