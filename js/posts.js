2c816b96a31dd245ec3e4344f48c6e692ac2a0c4
grez911 <grez911@gmail.com>
2017-10-24 21:02:07 +0300

    Returned ascending posts order

--- js/posts.js
+++ js/posts.js
@@ -3,7 +3,6 @@ Vue.component('posts', {
         <div>
             <newpost></newpost>
             <div v-for="post in posts" class="post">
-                <!--{{ post['post_id'] }}-->
                 <post :post_id="post.post_id"></post>
                 <hr>
             </div>
@@ -25,79 +24,39 @@ Vue.component('posts', {
 
     methods: {
         load() {
-            page.cmdp('dbQuery', ["SELECT post_id FROM post ORDER BY date_published DESC"]).then((res) => {
+            page.cmdp('dbQuery', ["SELECT post_id FROM post ORDER BY date_published ASC"]).then((res) => {
                 this.posts = res
             })
         }
     }
 })
 
-//Vue.component('post', {
-    //template: `
-        //<div class="columns">
-            //<div class="column is-8 post-body">
-                //{{ post.body }}
-            //</div>
-            //<div class="column is-4 post-comments">
-                //<likes :post_id="post.post_id" :date_published="post.date_published"></likes>
-                //<comments :post_id="post.post_id"></comments>
-            //</div>
-        //</div>
-    //`,
-
-    //props: ['post_id'],
-
-    //data() {
-        //return {
-            //post: {
-                //post_id: this.post_id,
-                //body: null,
-                //date_published: null
-            //}
-        //}
-    //},
-
-    //mounted() {
-        //this.load()
-        //bus.$on('update', () => {
-            //this.load()
-        //})
-    //},
-
-    //methods: {
-        //load() {
-            //page.cmdp('dbQuery', ["SELECT * FROM post WHERE post_id=" + this.post_id]).then((res) => {
-                //this.post = res[0]
-            //})
-        //}
-    //}
-//})
-
 Vue.component('post', {
     template: `
-        <div>
-            {{ post_id }} {{ post.body }}
+        <div class="columns">
+            <div class="column is-8 post-body">
+                {{ post.body }}
+            </div>
+            <div class="column is-4 post-comments">
+                <likes :post_id="post.post_id" :date_published="post.date_published"></likes>
+                <comments :post_id="post.post_id"></comments>
+            </div>
         </div>
     `,
+
     props: ['post_id'],
 
     data() {
         return {
             post: {
-                post_id: null,
+                post_id: this.post_id,
                 body: null,
                 date_published: null
             }
         }
     },
 
-    //updated() {
-        //console.log(this.post_id)
-        //this.load()
-    //},
-
     mounted() {
-        console.log(this.post_id)
         this.load()
         bus.$on('update', () => {
             this.load()
@@ -107,7 +66,6 @@ Vue.component('post', {
     methods: {
         load() {
             page.cmdp('dbQuery', ["SELECT * FROM post WHERE post_id=" + this.post_id]).then((res) => {
-                //console.log(res[0])
                 this.post = res[0]
             })
         }
