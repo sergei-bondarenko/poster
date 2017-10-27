9b452de7ad1f70652400dede2710ce6701240a44
grez911 <grez911@gmail.com>
2017-10-27 13:36:55 +0300

    Like/unlike now working

--- js/likes.js
+++ js/likes.js
@@ -1,8 +1,8 @@
 Vue.component('likes', {
     template: `
         <div>
-            <p class="is-size-7"><a :href="getURL()"><!--{{ date_published }}--></a></p>
-            <i class="fa" :class="{'fa-heart': liked, 'fa-heart-o': !liked}" aria-hidden="true" @click="like"></i>
+            <p class="is-size-7"><a :href="url">{{ date_published }}</a></p>
+            <i class="fa" :class="{'fa-heart': liked, 'fa-heart-o': !liked}" aria-hidden="true" @click="like()"></i>
             <p>{{ likes }}</p>
         </div>
     `,
@@ -18,74 +18,23 @@ Vue.component('likes', {
 
         liked() {
             if (this.post.post_id in storage.state.likes) {
-                return storage.state.likes[this.post.post_id].indexOf(storage.state.site_info.auth_address) != -1
+                return storage.state.likes[this.post.post_id]
+                    .indexOf(storage.state.site_info.auth_address) != -1
             }
-        }
-    },
+        },
 
-    mounted() {
-        //this.load()
-        bus.$on('update_likes', () => {
-            this.load()
-        })
+        date_published() {
+            return this.post.date_published
+        },
+
+        url() {
+            return '?post=' + this.post.post_id
+        },
     },
 
     methods: {
-        getURL() {
-            return '?post=' + this.post_id
-        },
-
-        load() {
-            page.cmdp('siteInfo', []).then((site_info) => {
-                page.site_info = site_info
-                return page.cmdp('dbQuery', ["SELECT count(*) AS likes from post_vote WHERE post_id=" + this.post_id])
-            }).then((res) => {
-                this.likes = res[0].likes
-                return page.cmdp('dbQuery', ["SELECT 'post_vote' AS type, post_id AS uri "
-                  + "FROM json LEFT JOIN post_vote USING (json_id) "
-                  + "WHERE directory = 'users/" + page.site_info.auth_address + "' "
-                  + "AND file_name = 'data.json' "
-                  + "AND uri=" + this.post_id])
-            }).then((res) => {
-                if (res.length > 0) {
-                    this.liked = true
-                } else {
-                    this.liked = false
-                }
-            })
-        },
-
         like() {
-            page.cmdp('siteInfo', []).then((site_info) => {
-                page.site_info = site_info
-                if (page.site_info.cert_user_id == null) {
-                    page.cmdp('certSelect', {'accepted_domains': ['zeroid.bit']})
-                    return Promise.reject("Certificate is not selected")
-                } else {
-                    return page.cmdp('fileGet', ['data/users/' + page.site_info.auth_address + '/data.json'])
-                }
-            }).then((data) => {
-                if (data) {
-                    data = JSON.parse(data)
-                } else {
-                    data = {
-                        'next_comment_id': 1,
-                        'comment': [],
-                        'comment_vote': {},
-                        'post_vote': {}
-                    }
-                }
-
-                if (this.liked) {
-                    delete data.post_vote[this.post_id]
-                } else {
-                    data.post_vote[this.post_id] = 1
-                }
-
-                page.writePublish('data/users/' + page.site_info.auth_address + '/data.json', data)
-            }).catch((err) => {
-                console.log(err)
-            })
+            page.like(this.post.post_id)
         }
     }
 })
--- js/page.js
+++ js/page.js
@@ -4,8 +4,17 @@ class Page extends ZeroFrame {
         this.site_info = null
     }
 
+    get isCertSelected() {
+        if (this.site_info.cert_user_id == null) {
+            this.cmdp('certSelect', {'accepted_domains': ['zeroid.bit']})
+            return false
+        } else {
+            return true
+        }
+    }
+
     onRequest(cmd, message) {
-        if (cmd == "setSiteInfo") {
+        if (cmd == 'setSiteInfo') {
             storage.commit('loadSiteInfo')
         }
     }
@@ -13,7 +22,6 @@ class Page extends ZeroFrame {
     writePublish(inner_path, data) {
         let json_raw = unescape(encodeURIComponent(JSON.stringify(data, undefined, '    ')))
         page.cmdp('fileWrite', [inner_path, btoa(json_raw)]).then(() => {
-            bus.$emit('update')
             if (inner_path == 'data/data.json') {
                 return page.cmdp('sitePublish', ['stored'])
             } else {
@@ -22,16 +30,32 @@ class Page extends ZeroFrame {
         })
     }
 
-    checkCert() {
-        page.cmdp('siteInfo', []).then((site_info) => {
-            this.site_info = site_info
-            if (this.site_info.cert_user_id == null) {
-                this.cmdp('certSelect', {'accepted_domains': ['zeroid.bit']})
-                return Promise.reject("Certificate is not selected")
+    async getDataJson() {
+        return await this.cmdp('fileGet', ['data/users/' + storage.state.site_info.auth_address + '/data.json'])
+    }
+
+    async like(post_id) {
+        if (this.isCertSelected) {
+            let data = await this.getDataJson()
+            if (data) {
+                data = JSON.parse(data)
             } else {
-                return this.cmdp('fileGet', ['data/users/' + this.site_info.auth_address + '/data.json'])
+                data = {
+                    'next_comment_id': 1,
+                    'comment': [],
+                    'comment_vote': {},
+                    'post_vote': {}
+                }
             }
-        })
+
+            if (post_id in data.post_vote) {
+                delete data.post_vote[post_id]
+            } else {
+                data.post_vote[post_id] = 1
+            }
+            await this.writePublish('data/users/' + storage.state.site_info.auth_address + '/data.json', data)
+            storage.commit('loadLikes')
+        }
     }
 
     async sqlQuery(query) {
