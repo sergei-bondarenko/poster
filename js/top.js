23921e9f62d1ca5dfb9d0e647d59ca9455cc668e
grez911 <grez911@gmail.com>
2017-10-28 17:50:35 +0300

    Started working on top.js

--- js/top.js
+++ js/top.js
@@ -15,7 +15,27 @@ Vue.component('top', {
             </section>
             <div class="container">
                 <div id="top-posts">
-                    Most liked | Last commented
+                    <!-- Most liked | Last commented -->
+                    <div class="dropdown is-active">
+                        <div class="dropdown-trigger">
+                        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <span>Most liked</span>
+                            <span class="icon is-small">
+                            <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                        </button>
+                        </div>
+                        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                            <div class="dropdown-content">
+                                <a href="#" class="dropdown-item">Past day</a>
+                                <a href="#" class="dropdown-item">Past week</a>
+                                <a href="#" class="dropdown-item">Past month</a>
+                                <a href="#" class="dropdown-item">Past year</a>
+                                <a href="#" class="dropdown-item">All time</a>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="button">Last commented</button>
                 </div>
             </div>
         </div>
