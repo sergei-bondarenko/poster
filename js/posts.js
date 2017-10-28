39ac271a3885c4ca20b0efe156620d905c3075cc
grez911 <grez911@gmail.com>
2017-10-28 16:14:35 +0300

    Added dynamic post loading when scrolling

--- js/posts.js
+++ js/posts.js
@@ -1,7 +1,7 @@
 Vue.component('posts', {
     template: `
         <div>
-            <div v-for="post in posts" class="post">
+            <div v-for="(post, index) in posts" class="post" :ref="'post'+index">
                 <div class="columns">
                     <div class="column is-8 post-body">
                         {{ post.body }}
@@ -16,9 +16,29 @@ Vue.component('posts', {
         </div>
     `,
 
+    data: () => {
+        return {
+            post_count: 15
+        }
+    },
+
     computed: {
         posts() {
-            return storage.state.posts
+            return storage.state.posts.slice(0, this.post_count)
+        }
+    },
+
+    mounted() {
+        window.addEventListener('scroll', this.scroll)
+    },
+
+    methods: {
+        scroll() {
+            let current_position = window.scrollY
+            let trigger_position = this.$refs['post' + (this.post_count-10)][0].offsetTop
+            if (current_position > trigger_position) {
+                this.post_count += 15
+            }
         }
     }
 })
