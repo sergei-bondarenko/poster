8a9b95655abce8a5370fd225c8d89c63d5536abd
grez911 <grez911@gmail.com>
2017-11-01 11:31:34 +0300

    Working on comments

--- js/comments.js
+++ js/comments.js
@@ -1,6 +1,49 @@
 Vue.component('comments', {
     template: `
         <div>
+            <article class="media" v-for="comment in comments">
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong :title="userTitle(comment)" v-text="cropIdProvider(comment.cert_user_id)">Barbara Middleton</strong>
+                            <br>
+                            <div class="wrapped"
+                                @blur="cancel"
+                                :contentEditable="comment_id == comment.comment_id"
+                                :ref="'comment' + comment.comment_id">{{ comment.body }}</div>
+                            <br>
+                            <a class="button is-danger" v-if="comment_id == comment.comment_id" @click="del(comment.comment_id)">Delete</a>
+                            <a class="button" v-if="comment_id == comment.comment_id">Save</a>
+                            <br v-if="comment_id == comment.comment_id">
+                            <br v-if="comment_id == comment.comment_id">
+                            <small><a @click="reply(comment.cert_user_id)">Reply</a> · {{ comment.date_added | fromNow }}</small>
+                        </p>
+                    </div>
+                </div>
+                <div class="media-right">
+                    <span class="pointer" v-if="own(comment.cert_user_id)" @click="edit(comment)"><i class="fa pointer fa-pencil"></i></span>
+                </div>
+            </article>
+            <article class="media">
+                <div class="media-content">
+                    <div class="field">
+                        <p class="control">
+                            <textarea class="textarea" placeholder="Add a comment..." v-model="commentText"></textarea>
+                        </p>
+                    </div>
+                    <div class="field">
+                        <p class="control">
+                            <button class="button" @click="save">Post comment</button>
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    `,
+
+
+/*
+        <div>
             <div v-for="comment in comments" class="comment">
                 <div class="comment-info">
                     <span class="comment-username" :title="userTitle(comment)" v-text="cropIdProvider(comment.cert_user_id)"></span>
@@ -17,14 +60,13 @@ Vue.component('comments', {
             <textarea class="comment-textarea" v-model="commentText"></textarea>
             <button type="button" @click="save">Save</button>
         </div>
-    `,
-
+*/
     props: ['post'],
 
     data() {
         return {
             commentText: '',
-            comment_id: null
+            comment_id: null,
         }
     },
 
@@ -66,12 +108,22 @@ Vue.component('comments', {
         },
 
         del(id) {
+            console.log(1)
             poster.delComment(id)
         },
 
         edit(comment) {
             this.commentText = comment.body
             this.comment_id = comment.comment_id
+            this.$nextTick(() => {
+                this.$refs['comment' + comment.comment_id][0].focus()
+            })
+        },
+
+        cancel() {
+            this.commentText = ''
+            this.comment_id = null
+            // MAKE POSTS RELOAD BECAUSE CHANGED CONTENT WILL PERSIST
         }
     }
 })
